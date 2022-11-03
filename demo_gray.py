b62a820d5056d45e3458a64992cffb9c75223bc5
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2022-11-03 13:07:57 +0300

    Add try except

--- demo_gray.py
+++ demo_gray.py
@@ -11,7 +11,7 @@ from fe_tools.fff_tools import get_thermal_image_vis_gray
 
 
 def main():
-    main_dir = "erg_kz/1_day"
+    main_dir = "/data/sinitsin/erg_kz/1_day"
     for seq_file in os.listdir(main_dir):
         if seq_file.endswith(".seq"):
             print(f"Ectracting {seq_file}:")
@@ -26,16 +26,20 @@ def extract_seq_gray(seq_path: str) -> None:
 
     thermal_tensor = []
     for frame_id, frame_bytes in enumerate(tqdm(Seq(seq_path))):
-        frame_path = f"{folder}/fff_frames/{frame_id}.fff"
+        frame_path = f"{folder}/fff_frames/{frame_id:04d}.fff"
         with open(frame_path, "wb") as fff_file:
             fff_file.write(frame_bytes)
 
-        thermal_image = get_thermal_image(frame_path)
+        try:
+            thermal_image = get_thermal_image(frame_path)
+        except Exception as e:
+            print(f"Error: {e}")
+            continue
         os.remove(frame_path)
         thermal_tensor.append(thermal_image)
-        frame_path = f"{folder}/thermal_frames/{folder.split('/')[-1]}_{frame_id}.jpg"
+        frame_path = f"{folder}/thermal_frames/{folder.split('/')[-1]}_{frame_id:04d}.jpg"
         cv2.imwrite(frame_path, get_thermal_image_vis_gray(thermal_image))
-    os.rmdir(f"{folder}/fff_frames")
+    shutil.rmtree(f"{folder}/fff_frames")
     thermal_tensor = np.array(thermal_tensor)
     print(f"Saving thermal tensor {thermal_tensor.shape} ...")
     # np.save(f"{folder}/thermal.npy", thermal_tensor)
