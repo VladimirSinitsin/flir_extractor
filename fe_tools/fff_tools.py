08408fe579ade7bf102477f7d87f678a7d6473b8
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2022-11-18 20:34:04 +0300

    Edit `get_thermal_image_vis_gray`

--- fe_tools/fff_tools.py
+++ fe_tools/fff_tools.py
@@ -84,10 +84,10 @@ def get_thermal_image_vis(thermal_np: np.ndarray) -> np.ndarray:
     return thermal_img_vis
 
 
-def get_thermal_image_vis_gray(thermal_np: np.ndarray) -> np.ndarray:
+def get_thermal_image_vis_gray(thermal_np: np.ndarray, min_thr: float, max_thr: float) -> np.ndarray:
     th_np = thermal_np.copy()
     # th_np -= np.min(th_np)
     # th_np -= 200
-    th_np = np.clip(th_np, 0, 500)
+    th_np = np.clip(th_np, min_thr, max_thr)
     thermal_img_vis = (th_np / np.max(th_np) * 255).astype("uint8")
     return thermal_img_vis
